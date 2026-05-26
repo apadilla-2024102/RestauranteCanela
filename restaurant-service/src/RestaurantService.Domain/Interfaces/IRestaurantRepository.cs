132267556a669fcc022145c45930f27adfb3603b
apadilla-2024102 <apadilla-2024102@kinal.edu.gt>
2026-05-26 02:24:31 -0600

    Se modifico AuthService

--- restaurant-service/src/RestaurantService.Domain/Interfaces/IRestaurantRepository.cs
+++
@@ -1,9 +0,0 @@
-using RestaurantService.Domain.Entities;
-
-namespace RestaurantService.Domain.Interfaces;
-
-public interface IRestaurantRepository : IRepository<Restaurant>
-{
-    Task<IEnumerable<Restaurant>> GetActiveRestaurantsAsync();
-    Task<Restaurant?> GetByNameAsync(string name);
-}
