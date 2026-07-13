f0bb8590d612c904c0e577f3c3af718cfee5b1f2
apadilla-2024102 <apadilla-2024102@kinal.edu.gt>
2026-07-12 20:09:16 -0600

    Se modifico AT-002

--- auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
+++ auth-service/src/AuthService.Api/obj/Debug/net8.0/AuthService.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6619b792c50df410206d7665d5bb36f2f19babd1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthService.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
